ff670319fee83f683ca8323d61b3da43750b90b8
Jonathan Rico <jonathan_rico@softwarenextdoor.com>
2011-07-10 17:02:33 -0700

    Fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110710045904) do
+ActiveRecord::Schema.define(:version => 20110710234310) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -36,7 +36,5 @@ ActiveRecord::Schema.define(:version => 20110710045904) do
     t.string   "sfdc_token"
     t.string   "sfdc_refresh_token"
   end
-
-  #add_index "users", ["email"], :name => "index_users_on_email"
 
 end
